3f21aedc5e56ad09f03dac7eeb3049f129e334a4
PatrickGTR <patricksubang@live.com>
2019-07-06 15:53:21 +0100

    fix bug/typo in solo stats

--- components/stats/soloStats.js
+++ components/stats/soloStats.js
@@ -10,7 +10,9 @@ const SoloStats = ({ data }) => {
         <span className="list-item">K/D - {data.kd_solo}</span>
         <span className="list-item">Kills - {data.kills_solo}</span>
         <span className="list-item">Placed Top 1 - {data.placetop1_solo}</span>
-        <span className="list-item">Placed Top 3 - {data.placetop3_solo}</span>
+        <span className="list-item">
+          Placed Top 10 - {data.placetop10_solo}
+        </span>
         <span className="list-item">
           Placed Top 25 - {data.placetop25_solo}
         </span>
